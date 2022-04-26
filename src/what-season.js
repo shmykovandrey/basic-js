433b16422f1d4a627d24c05becff47027bb61232
Feomatar4eg <feomatar4eg@gmail.com>
2022-04-26 23:33:05 +0300

    feat: 6 of 9 what-seasons complite

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,6 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const {
+  NotImplementedError
+} = require('../extensions/index.js');
 
 /**
  * Extract season from given date and expose the enemy scout!
@@ -11,9 +13,51 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
+function getSeason(date) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  // console.log('date: ' + (date instanceof Date) + ' nan: ' + !isNaN(date.valueOf()))
+  // console.log('full ' + )
+  console.log(date);
+  console.log(Date.parse(date));
+
+
+  if (!(date instanceof Date)) {
+    throw new Error('Invalid date!');
+    return 'Unable to determine the time of year!'
+  }
+  if (!!date == false) {
+    throw new Error('Invalid date!');
+  }
+  let month = date.getMonth();
+  switch (month) {
+    case 11:
+    case 0:
+    case 1: {
+      return 'winter';
+      break;
+    }
+    case 2:
+    case 3:
+    case 4: {
+      return 'spring';
+      break;
+    }
+    case 5:
+    case 6:
+    case 7: {
+      return 'summer';
+      break;
+    }
+    case 8:
+    case 9:
+    case 10: {
+      return 'fall';
+      break;
+    }
+
+  }
+  return 'Unable to determine the time of year!';
 }
 
 module.exports = {
