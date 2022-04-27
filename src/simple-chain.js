0776086dad88ceffc8dffe8956c4d303831197e7
Feomatar4eg <feomatar4eg@gmail.com>
2022-04-27 21:41:31 +0300

    init: start simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,28 +1,46 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const {
+  NotImplementedError
+} = require('../extensions/index.js');
 
 /**
  * Implement chainMaker object according to task description
  * 
  */
 const chainMaker = {
+  allData: new Array,
   getLength() {
-    throw new NotImplementedError('Not implemented');
+    console.log('getLenght ' + this.allData);
+    return this.arr.length;
+    //throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
+  addLink(value) {
+    console.log(chainMaker.allData);
+    // if (typeof value !== 'number'){
+    //   throw
+    // }
+    console.log('addLink ' + this.allData + ' ' + value);
+    this.allData.push(value);
+    //throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
+  removeLink(position) {
+    console.log('remove ' + this.arr + ' ' + position);
+    if (position >= this.arr.length || (typeof position != 'number')) {
+      throw new Error('You can\'t remove incorrect link!')
+    }
+    this.arr.splice(position, 1);
+    //throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
+    this.arr.reverse();
+    //throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
+    return this.allData.forEach(elem => `( ${elem} )`).join('~~');
+    //throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
 };
