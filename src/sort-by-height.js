38bab5d3e157539a29dd4c0925935229294e4d10
Feomatar4eg <feomatar4eg@gmail.com>
2022-04-27 15:31:21 +0300

    feat: complite sort-by-height

--- src/sort-by-height.js
+++ src/sort-by-height.js
@@ -1,4 +1,6 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const {
+  NotImplementedError
+} = require('../extensions/index.js');
 
 /**
  * Given an array with heights, sort them except if the value is -1.
@@ -11,8 +13,27 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new NotImplementedError('Not implemented');
+function sortByHeight(arr) {
+
+  let newArr = []
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === -1) newArr.push(i);
+  }
+  arr = arr.filter(elem => elem !== -1).sort((a, b) => a - b);
+  newArr.forEach(elem => {
+    arr.splice(elem, 0, -1)
+  })
+  return arr;
+
+
+  // return arr.sort((a, b) => {
+  //   if (a == -1 || b == -1) {
+  //     return 0
+  //   } else {
+  //     return a - b
+  //   }
+  // })
+  //  throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
 }
 
