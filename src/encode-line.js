be8e0cc7d6c1cedb958c57e03dd475411ecac4ef
Feomatar4eg <feomatar4eg@gmail.com>
2022-04-29 01:12:01 +0300

    feat: complite encode-line

--- src/encode-line.js
+++ src/encode-line.js
@@ -1,4 +1,6 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const {
+  NotImplementedError
+} = require('../extensions/index.js');
 
 /**
  * Given a string, return its encoding version.
@@ -10,10 +12,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function encodeLine(str) {
+  let count = 1;
+  let resultStr = '';
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === str[i + 1]) {
+      count++;
+    } else if (count > 1) {
+      resultStr += count + str[i];
+      count = 1;
+    } else resultStr += str[i];
+  }
+  return resultStr;
 }
+
 
 module.exports = {
   encodeLine
