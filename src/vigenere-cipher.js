63008bd11a7390190f1b0a506cf9df96d6c5a2ae
Feomatar4eg <feomatar4eg@gmail.com>
2022-04-29 16:29:41 +0300

    feat: complite vigener-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -22,19 +22,86 @@ const {
  * 
  */
 class VigenereCipheringMachine {
-  // constructor(message, key) {
-  //   this.message = message;
-  //   this.key = key;
-  // }
+  constructor(direct = true) {
+    this.direct = direct;
+  }
   encrypt(message, key) {
     if (!!message == false || !!key == false) {
-      throw new Error('Incorrect arguments!')
+      throw new Error('Incorrect arguments!');
     }
+    message = message.toLowerCase();
+    key = key.toLowerCase();
+    let vigenerMatrix = [];
+    let alphabet = 'abcdefghijklmnopqrstuvwxyz';
+    let arrAlphabet = alphabet.split('');
+    for (let i = 0; i < alphabet.length; i++) {
+      vigenerMatrix.push(arrAlphabet.join(''));
+      let char = arrAlphabet.shift();
+      arrAlphabet.push(char);
+    }
+    while (key.length < message.length) {
+      key = key + key;
+    }
+    message.split('').forEach((elem, index) => {
+      if (vigenerMatrix[0].indexOf(elem) == -1) {
+        key = key.split('')
+        key.splice(index, 0, elem)
+        key = key.join('');
+      }
+    })
+    key = key.substr(0, message.length)
+    // console.log('message = ' + message)
+    // console.log('key = ' + key);
+    let cipherStr = '';
+    for (let i = 0; i < key.length; i++) {
+      if (vigenerMatrix[0].indexOf(message[i]) == -1) {
+        cipherStr += message[i];
+      } else if (vigenerMatrix[0].indexOf(key[i]) !== -1) {
+        cipherStr += vigenerMatrix[vigenerMatrix[0].indexOf(key[i])][vigenerMatrix[0].indexOf(message[i])];
+      }
+    }
+    // console.log(cipherStr.toUpperCase())
+    return (this.direct) ? cipherStr.toUpperCase() : cipherStr.split('').reverse().join('').toUpperCase()
   }
   decrypt(message, key) {
     if (!!message == false || !!key == false) {
       throw new Error('Incorrect arguments!')
     }
+    message = message.toLowerCase();
+    key = key.toLowerCase();
+    let vigenerMatrix = [];
+    let alphabet = 'abcdefghijklmnopqrstuvwxyz';
+    let arrAlphabet = alphabet.split('');
+    for (let i = 0; i < alphabet.length; i++) {
+      vigenerMatrix.push(arrAlphabet.join(''));
+      let char = arrAlphabet.shift();
+      arrAlphabet.push(char);
+    }
+    while (key.length < message.length) {
+      key = key + key;
+    }
+    message.split('').forEach((elem, index) => {
+      if (vigenerMatrix[0].indexOf(elem) == -1) {
+        key = key.split('')
+        key.splice(index, 0, elem)
+        key = key.join('');
+      }
+    })
+    key = key.substr(0, message.length)
+    // console.log('message = ' + message)
+    // console.log('key = ' + key);
+    let unCipherStr = '';
+    for (let i = 0; i < key.length; i++) {
+      if (vigenerMatrix[0].indexOf(message[i]) == -1) {
+        unCipherStr += message[i];
+      } else {
+        let rowNum = vigenerMatrix[0].indexOf(key[i])
+        let colNum = vigenerMatrix[rowNum].indexOf(message[i])
+        unCipherStr += vigenerMatrix[0][colNum];
+      }
+    }
+    // console.log('----------' + unCipherStr.toUpperCase());
+    return (this.direct) ? unCipherStr.toUpperCase() : unCipherStr.split('').reverse().join('').toUpperCase()
   }
 }
 
