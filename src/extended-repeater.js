7c815fb91334bb335edfbecb785814e172f87e77
Feomatar4eg <feomatar4eg@gmail.com>
2022-04-29 12:01:25 +0300

    feat: complite extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,4 +1,6 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const {
+  NotImplementedError
+} = require('../extensions/index.js');
 
 /**
  * Create a repeating string based on the given parameters
@@ -15,9 +17,33 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  options.hasOwnProperty('repeatTimes') || (options.repeatTimes = 1);
+
+  if (!options.hasOwnProperty('additionRepeatTimes')) {
+    options.hasOwnProperty('addition') ? (options.additionRepeatTimes = 1) : (options.additionRepeatTimes = 0);
+  }
+  options.hasOwnProperty('addition') || (options.addition = '');
+  options.hasOwnProperty('additionSeparator') || (options.additionSeparator = '|');
+  options.hasOwnProperty('separator') || (options.separator = '+');
+  let newStr = '';
+  if (Object.keys(options).length == 0) {
+    return str;
+  } else if (options.repeatTimes == 0) {
+    return str;
+  } else {
+    for (let i = 1; i <= options.repeatTimes; i++) {
+      newStr = newStr + str;
+      if (options.additionRepeatTimes > 0) {
+        for (let j = 1; j <= options.additionRepeatTimes; j++) {
+          newStr += options.addition;
+          if (j < options.additionRepeatTimes) newStr += options.additionSeparator;
+        }
+      }
+      if (i < options.repeatTimes) newStr += options.separator;
+    }
+  }
+  return newStr;
 }
 
 module.exports = {
